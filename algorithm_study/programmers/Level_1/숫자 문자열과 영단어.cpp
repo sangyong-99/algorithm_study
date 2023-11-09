696a1168714e994e9683962e70718b58b51f5136
new_dragon <tkddyd1126@gmail.com>
2023-11-09 10:36:03 +0900

    11/9

---
+++ algorithm_study/programmers/Level_1/숫자 문자열과 영단어.cpp
@@ -0,0 +1,34 @@
+//
+//  숫자 문자열과 영단어.cpp
+//  algorithm_study
+//
+//  Created by 신상용 on 11/9/23.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <regex>
+
+using namespace std;
+
+vector<string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+int solution(string s) {
+    int answer = 0;
+    regex r;
+    for(int i = 0; i < numbers.size(); i++) {
+        r = numbers[i];
+        s = regex_replace(s, r, to_string(i));
+    }
+    answer = stoi(s);
+    return answer;
+}
+
+int main() {
+    cout << solution("23fourfourfourfour5six7");
+}
