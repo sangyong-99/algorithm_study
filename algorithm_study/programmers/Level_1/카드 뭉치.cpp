327afdb90d4984937b7919048a0a5d841e8afd33
new_dragon <tkddyd1126@gmail.com>
2023-12-07 21:41:43 +0900

    달리기 경주(2)

--- algorithm_study/programmers/Level_1/카드 뭉치.cpp
+++ algorithm_study/programmers/Level_1/카드 뭉치.cpp
@@ -5,4 +5,26 @@
 //  Created by 신상용 on 11/30/23.
 //
 
-#include <stdio.h>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+    
+    unsigned int repeatCount = goal.size();
+    for(int i = 0; i < repeatCount; i++) {
+        if (goal.front() == cards1.front()) {
+            cards1.erase(cards1.begin());
+            goal.erase(goal.begin());
+        }
+        else if (goal.front() == cards2.front()) {
+            cards2.erase(cards2.begin());
+            goal.erase(goal.begin());
+        }
+        else return "No";
+    }
+    return "Yes";
+}
