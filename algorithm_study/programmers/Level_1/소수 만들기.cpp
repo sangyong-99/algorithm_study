0e7e147d49dcbb182091ad0cdae998ee45ed37de
new_dragon <tkddyd1126@gmail.com>
2023-12-11 09:45:16 +0900

    소수 만들기

--- algorithm_study/programmers/Level_1/소수 만들기.cpp
+++ algorithm_study/programmers/Level_1/소수 만들기.cpp
@@ -5,4 +5,42 @@
 //  Created by 신상용 on 12/10/23.
 //
 
-#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    bool decimal[3000];
+    
+    for(int i = 0; i <= 3000; i++) {
+        decimal[i] = true;
+    }
+    
+    for(int i = 2; i <= 3000; i++) {
+        if (decimal[i]) {
+            cout << i << " ";
+            for(int j = i * 2; j <= 3000; j += i) {
+                decimal[j] = false;
+            }
+        }
+    }
+    
+    for(int i = 0; i < nums.size() - 2; i++) {
+        for(int j = i + 1; j < nums.size() - 1; j++) {
+            for(int z = j + 1; z < nums.size(); z++) {
+                if (decimal[nums[i] + nums[j] + nums[z]]) {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    
+    
+
+    return answer;
+}
