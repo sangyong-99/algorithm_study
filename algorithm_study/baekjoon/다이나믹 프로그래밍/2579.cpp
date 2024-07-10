5826870c191203481b836abaf2a628e3b1a71f7a
new_dragon <tkddyd1126@gmail.com>
2024-07-10 12:17:06 +0900

    2579.cpp

---
+++ algorithm_study/baekjoon/다이나믹 프로그래밍/2579.cpp
@@ -0,0 +1,29 @@
+//
+//  2579.cpp
+//  algorithm_study
+//
+//  Created by 신상용 on 7/10/24.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, datas[310][3];
+
+int main() {
+    cin >> N;
+    for(int i = 1; i <= N; i++) {
+        cin >> datas[i][0];
+    }
+    
+    datas[1][1] = datas[1][0];
+    datas[2][1] = datas[1][1] + datas[2][0];
+    datas[2][2] = datas[2][0];
+    
+    for(int i = 3; i <= N; i++) {
+        datas[i][1] = datas[i-1][2] + datas[i][0];
+        datas[i][2] = max(datas[i-2][1], datas[i-2][2]) + datas[i][0];
+    }
+    
+    cout << max(datas[N][1], datas[N][2]);
+}
