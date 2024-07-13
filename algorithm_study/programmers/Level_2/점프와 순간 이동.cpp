ddcaae7373f53282a2e451a38bbd64ce872c1ed2
new_dragon <tkddyd1126@gmail.com>
2024-07-13 10:52:57 +0900

    점프와 순간이동

---
+++ algorithm_study/programmers/Level_2/점프와 순간 이동.cpp
@@ -0,0 +1,25 @@
+//
+//  점프와 순간 이동.cpp
+//  algorithm_study
+//
+//  Created by Shin Sangyong on 7/13/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while (n != 0) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n -= 1;
+            ans++;
+        }
+    }
+
+    return ans;
+}
