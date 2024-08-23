eb0b73cd9478e018455eba455520cb3bdad08b4d
new_dragon <tkddyd1126@gmail.com>
2024-08-24 00:19:53 +0900

    15665.cpp

---
+++ algorithm_study/baekjoon/백트래킹/15665.cpp
@@ -0,0 +1,45 @@
+//
+//  15665.cpp
+//  algorithm_study
+//
+//  Created by 신상용 on 8/24/24.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, temp;
+vector<int> datas;
+int arr[10], used[10];
+
+void input_data() {
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        cin >> temp;
+        datas.push_back(temp);
+    }
+    sort(datas.begin(), datas.end());
+}
+
+void func(int k) {
+    if (k == M) {
+        for(int i = 0; i < M; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int standard = 0;
+    for(int i = 0; i < N; i++) {
+        if (standard != datas[i]) {
+            standard = datas[i];
+            arr[k] = datas[i];
+            func(k+1);
+        }
+    }
+}
+
+int main() {
+    input_data();
+    func(0);
+}
